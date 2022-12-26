6b9bec5acbe26bd4b7f80fed701668c0748212f0
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 23:11:35 +0100

    feat: added logic for forget password and reset passowrd routes

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from 'uuid';
-import { compare, hashing } from "../helpers";
+import { compare, hashing, sendMail } from "../helpers";
 import { Request, Response } from "express";
 import User, { IUser } from "../models/user";
 const jwtSecret = process.env.JWT_SECRET;
@@ -10,6 +10,7 @@ interface Req extends Request {
     email: string;
     password: string;
     name?: string;
+    user_id?: string;
   };
 }
 const login = async (req: Req, res: Response) => {
@@ -59,4 +60,44 @@ const signUp = async (req: Req, res: Response) => {
     res.status(400).json({ error: true, message: "Something went wrong" });
   }
 };
-export { login, signUp };
+
+const forgotPassword = async (req: Req, res: Response) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ error: true, message: "User not found" });
+  } else {
+    sendMail({
+      to: email,
+      subject: "Password Reset",
+      text: "I hope this message gets sent!",
+    }, (err: any, info: any) => {
+      if (err) {
+        res.status(500).json({ error: true, message: "Something went wrong" });
+      }
+      else {
+        res.status(200).json({ error: false, message: "Password reset link sent" });
+      }
+    })
+  }
+}
+
+const resetPassword = async (req: Req, res: Response) => {
+  const { user_id, password } = req.body;
+  const user = await User.findOne({
+    id: user_id,
+  });
+  if (!user) {
+    return res.status(400).json({ error: true, message: "User not found" });
+  }
+  const hashedPassword = await hashing(password) as string;
+  if (hashedPassword) {
+    user.password = hashedPassword;
+    await user.save();
+    res.status(200).json({ error: false, message: "Password reset successful" });
+  } else {
+    res.status(400).json({ error: true, message: "Something went wrong" });
+  }
+}
+
+export { login, signUp, resetPassword, forgotPassword };
