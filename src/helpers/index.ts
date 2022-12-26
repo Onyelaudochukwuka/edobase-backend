778ad0c038cf3160222cd1b09dded41c0415bb1c
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 20:57:24 +0100

    feat: created helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,6 @@
 import { createHmac } from 'node:crypto';
 
-const hash = (str: string): string | boolean => {
+function hashing (str: string): string | boolean {
   if (typeof str === 'string' && str.length > 0) {
     const hash = createHmac('sha256', '0af8c5b760970bdc5aca7bbcb899b32d118a274fb3930e7340d8d40fd85a17eee44b3f9f1f5664f40ae243c121c292be576517dbaea54e9901d103028359ce5d')
       .update(str)
@@ -10,4 +10,6 @@ const hash = (str: string): string | boolean => {
     return false
   }
 };
-export { hash };
+
+const compare = (str: string, hash: string): boolean => (hashing(str.toString()) === hash);
+export { hashing, compare };
