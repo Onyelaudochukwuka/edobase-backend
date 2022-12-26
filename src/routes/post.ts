1779626ddfe037b75c03ee3bbd071aa478ba73ec
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 20:56:52 +0100

    feat: created routes

---
+++ src/routes/post.ts
@@ -0,0 +1,9 @@
+import express, { Response, Request, NextFunction, IRouter } from 'express';
+const router: IRouter = express.Router();
+
+/* GET home page. */
+router.get('/', function (req: Request, res: Response , next: NextFunction) {
+  res.json({ title: 'Express' });
+});
+
+export default router;
