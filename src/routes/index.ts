00364da961baf2e562cac688a0ccf2c830df93d5
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 20:57:02 +0100

    feat: exported routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,4 @@
-import express, { Response, Request, NextFunction, IRouter } from 'express';
-const router: IRouter = express.Router();
-
-/* GET home page. */
-router.get('/', function (req: Request, res: Response , next: NextFunction) {
-  res.json({ title: 'Express' });
-});
-
-export default router;
+// Export all routes
+export { default as auth } from './auth';
+export { default as admin } from './admin';
+export { default as post } from './post';
