6a0670a0246461f62be913a9ce2431d4e6d71e31
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 22:08:55 +0100

    feat: editing controllers

---
+++ src/controllers/auth.ts
@@ -0,0 +1,62 @@
+import jwt from "jsonwebtoken";
+import { v4 as uuidv4 } from 'uuid';
+import { compare, hashing } from "../helpers";
+import { Request, Response } from "express";
+import User, { IUser } from "../models/user";
+const jwtSecret = process.env.JWT_SECRET;
+if (!jwtSecret) throw new Error("Secret hash is missing");
+interface Req extends Request {
+  body: {
+    email: string;
+    password: string;
+    name?: string;
+  };
+}
+const login = async (req: Req, res: Response) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }) as IUser;
+  if (!user) {
+    return res.status(400).json({  error: true, message: "User not found" });
+  }
+  const isMatch = await compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({  error: true, message: "Incorrect password" });
+  }
+  const token = jwt.sign({ userId: user.id }, jwtSecret || "", {
+    expiresIn: "30d",
+  });
+  res.json({ error: false, token, userId: user.id });
+};
+
+const signUp = async (req: Req, res: Response) => {
+  let { email, password, name } = req.body;
+  const user = await User.findOne({
+    email,
+  });
+  if (user) {
+    return res.status(400).json({ error: true, message: "User already exists" });
+  }
+  const id: string = uuidv4();
+  password = await hashing(password) as string;
+  console.log(id);
+  if (password) {
+    const newUser = new User({
+      email,
+      password,
+      name,
+      id
+    });
+    await newUser.save();
+    const token = jwt.sign({
+      userId: newUser
+    }, jwtSecret,
+      {
+        expiresIn: "30d"
+      }
+    );
+    res.status(201).json({ error: false, token, userId: newUser.id });
+  } else {
+    res.status(400).json({ error: true, message: "Something went wrong" });
+  }
+};
+export { login, signUp };
