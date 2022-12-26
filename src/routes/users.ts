1068a0776d85b4bb8270006ac43dc8b05eec6e8c
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 16:09:51 +0100

    feat: linting

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,6 @@
 import express, { Application, Response, Request, NextFunction, RouterOptions, IRouter } from 'express';
 const router: IRouter = express.Router();
+
 /* GET users listing. */
 router.get('/', function(req: Request, res: Response, next: NextFunction) {
   res.send('respond with a resource');
