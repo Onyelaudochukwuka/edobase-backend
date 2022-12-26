608864b79ccaf2ce865d67a50704eaeaa9d626e0
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 22:09:29 +0100

    feat: added secret

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,8 +1,9 @@
 import { createHmac } from 'node:crypto';
-
-function hashing (str: string): string | boolean {
+const secret = process.env.SECRET_HASH;
+if(!secret) throw new Error('Secret hash is missing');
+function hashing (str: string): string | false { 
   if (typeof str === 'string' && str.length > 0) {
-    const hash = createHmac('sha256', '0af8c5b760970bdc5aca7bbcb899b32d118a274fb3930e7340d8d40fd85a17eee44b3f9f1f5664f40ae243c121c292be576517dbaea54e9901d103028359ce5d')
+    const hash = createHmac('sha256', secret || '')
       .update(str)
       .digest('hex')
     return hash
