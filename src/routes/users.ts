50b60a8968a1e68eccae12b181069f1908a93138
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 20:56:16 +0100

    feat: removed user route

--- src/routes/users.ts
+++
@@ -1,9 +0,0 @@
-import express, { Application, Response, Request, NextFunction, RouterOptions, IRouter } from 'express';
-const router: IRouter = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req: Request, res: Response, next: NextFunction) {
-  res.send('respond with a resource');
-});
-
-export default router;
