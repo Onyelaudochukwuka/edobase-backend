a8cb0ddabce0375e13c4bfd9e7a618f31acb0073
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 23:10:36 +0100

    feat: added middleware and validators

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,49 @@
-import express, { Application, Response, Request, NextFunction, RouterOptions, IRouter } from 'express';
-import { login, signUp } from "../controllers/auth";
+import express, {
+  Application,
+  Response,
+  Request,
+  NextFunction,
+  RouterOptions,
+  IRouter,
+} from "express";
+import {
+  forgotPassword,
+  login,
+  resetPassword,
+  signUp,
+} from "../controllers/auth";
+import { body } from "express-validator";
+import { Validate } from "../middleware";
 const router: IRouter = express.Router();
 
 /* GET users listing. */
-router.post('/login', login);
-router.post('/sign-up', signUp);
+router.post(
+  "/login",
+  body("email").isEmail().withMessage("Please enter a valid email."),
+  body("password").trim().isLength({ min: 5 }),
+  Validate,
+  login
+);
+router.post(
+  "/sign-up",
+  body("email").isEmail().withMessage("Please enter a valid email."),
+  body("password").trim().isLength({ min: 5 }),
+  body("name").trim().not().isEmpty(),
+  Validate,
+  signUp
+);
+router.post(
+  "/forgot-password",
+  body("email").isEmail().withMessage("Please enter a valid email."),
+  Validate,
+  forgotPassword
+);
+router.post(
+  "/reset-password",
+  body("user_id").trim().not().isEmpty(),
+  body("password").trim().isLength({ min: 5 }),
+  Validate,
+  resetPassword
+);
 
 export default router;
