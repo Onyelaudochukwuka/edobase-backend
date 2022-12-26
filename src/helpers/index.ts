475301f1ed19338f142751705a31131c3f915545
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 23:11:51 +0100

    feat: added helper for sms

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,16 +1,45 @@
-import { createHmac } from 'node:crypto';
+require('dotenv').config();
+import { createHmac } from "node:crypto";
+import nodemailer from "nodemailer";
+import aws from "@aws-sdk/client-ses";
+import { defaultProvider } from "@aws-sdk/credential-provider-node";
 const secret = process.env.SECRET_HASH;
-if(!secret) throw new Error('Secret hash is missing');
-function hashing (str: string): string | false { 
-  if (typeof str === 'string' && str.length > 0) {
-    const hash = createHmac('sha256', secret || '')
-      .update(str)
-      .digest('hex')
-    return hash
-  } else {
-    return false
-  }
-};
+if (!secret) throw new Error("Secret hash is missing");
 
-const compare = (str: string, hash: string): boolean => (hashing(str.toString()) === hash);
-export { hashing, compare };
+// create Nodemailer SES transporter
+let transporter = nodemailer.createTransport({
+  SES: { aws },
+});
+
+function hashing(str: string): string | false {
+  if (typeof str === "string" && str.length > 0) {
+    const hash = createHmac("sha256", secret || "")
+      .update(str)
+      .digest("hex");
+    return hash;
+  } else {
+    return false;
+  }
+}
+
+function sendMail({ to, subject, text }: { to: string, subject: string, text: string }, callback: any): void {
+  transporter.sendMail(
+    {
+      from: "udochukwukaonyela@gmail.com",
+      to,
+      subject,
+      text,
+    },
+    (err, info) => {
+      if (err) {
+        callback(true, err);
+      } else {
+        callback(false, info);
+      }
+    }
+  );
+}
+
+const compare = (str: string, hash: string): boolean => hashing(str.toString()) === hash;
+
+export { hashing, compare, sendMail };
