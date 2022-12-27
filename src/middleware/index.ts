2febdb627f0e2888accaa5c44e42a93f9ac8a339
Udoka <udochukwukaonyela@gmail.com>
2022-12-27 07:30:44 +0100

    feat: configured middlewares

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,34 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from "express"
+import { verify } from "jsonwebtoken";
 import { validationResult } from "express-validator";
+interface Req extends Request {
+  body: {
+    email: string;
+    password: string;
+    name?: string;
+    user_id?: string;
+  };
+  session?: any;
+}
+const jwtSecret = process.env.JWT_SECRET;
+if (!jwtSecret) throw new Error("Secret hash is missing");
+
+function isAuthrorized(req: Req, res: Response, next: NextFunction) {
+  if (req.session) {
+    verify(req.session.token, jwtSecret ?? '', (err: any, decoded: any) => {
+      if (err) {
+        res.status(401).send('Unauthorized')
+      }
+      if (decoded) {
+        console.log(decoded)
+        next()
+      }
+    })
+  } else {
+    res.status(401).send('Unauthorized')
+  }
+}
+
 
 function Validate( req: Request, res: Response, next: NextFunction ) {
   const errors = validationResult(req);
