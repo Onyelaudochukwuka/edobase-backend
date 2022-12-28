b1e69834e9240ec35184d829ad95079245bc667b
Udoka <udochukwukaonyela@gmail.com>
2022-12-28 23:41:31 +0100

    feat succesfully added logic for password reset and accound confirm

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { compare, hashing, sendMail } from "../helpers";
 import { Request, Response } from "express";
 import User, { IUser } from "../models/user";
@@ -12,18 +12,24 @@ interface Req extends Request {
     password: string;
     name?: string;
     user_id?: string;
+    client_id?: string;
+    LGA?: string;
+    phone?: string;
+    gender?: string;
+    description?: string;
+    username?: string;
   };
   session?: any;
 }
 const login = async (req: Req, res: Response) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email }) as IUser;
+  const user = (await User.findOne({ email })) as IUser;
   if (!user) {
-    return res.status(400).json({  error: true, message: "User not found" });
+    return res.status(400).json({ error: true, message: "User not found" });
   }
   const isMatch = await compare(password, user.password);
   if (!isMatch) {
-    return res.status(400).json({  error: true, message: "Incorrect password" });
+    return res.status(400).json({ error: true, message: "Incorrect password" });
   }
   const token = jwt.sign({ userId: user.id }, jwtSecret || "", {
     expiresIn: "30d",
@@ -39,117 +45,193 @@ const signUp = async (req: Req, res: Response) => {
     email,
   });
   if (user) {
-    return res.status(400).json({ error: true, message: "User already exists" });
+    return res
+      .status(400)
+      .json({ error: true, message: "User already exists" });
   }
   const id: string = uuidv4();
-  password = await hashing(password) as string;
+  password = (await hashing(password)) as string;
   console.log(id);
   if (password) {
     const newUser = new User({
       email,
       password,
       name,
-      id
+      id,
     });
     await newUser.save();
     const confirmation_id = uuidv4();
+    const ONE_DAY = 24 * 60 * 60 * 1000;
     const pass = new Pass({
       user_id: id,
       confirmation_id,
-    })
+      date: Date.now() + ONE_DAY,
+    });
     await pass.save();
-    // TODO send confirmation mail to email
-    sendMail({
-      to: email,
-      subject: "Password Reset",
-      value: `<div style='padding: 0;background-color: #FFDE4E;display: grid;place-items: center;font-family: 'Chivo Mono', monospace;'>
+    sendMail(
+      {
+        to: email,
+        subject: "Password Reset",
+        value: `<div style='padding: 0;background-color: #FFDE4E;display: grid;place-items: center;font-family: 'Chivo Mono', monospace;'>
   <h1 style='text-align: center;font-size: 35px;margin-bottom: 20px;'>Edobase</h1>
   <section style='background: white;width: 80%;margin: auto;height: fit-content;display: block;position: relative;padding: 25px;'>
     <p style='font-weight: 700'> Dear ${name},</p>
     <p>Thank you for signing up for our forum! We're excited to have you as a member of our community.<br />To complete your registration and activate your account, please click on the following link:</p>
-    <div><a href=''>https://edobase.vercel.app/complete/client_id=${confirmation_id}}</a></div>
+    <div><a href='https://edobase.vercel.app/complete/client_id=${confirmation_id}'>https://edobase.vercel.app/complete/client_id=${confirmation_id}</a></div>
     <div><code>The Link is valid for 24 hours <a href=''> Click here to generate another one</a></code></div>
     <p>Once you've clicked on the link, your account will be activated and you'll be able to start participating in discussions and connecting with other members.</p>
     <p>Thank you for joining us, and we look forward to seeing you on the forum!</p>
   </section>
 </div>
-</body>`
-    },(err: any, info: any) => {
-  if (err) {
-    res.status(500).json({ error: true, message: "Something went wrong" });
-  }
-  else {
-    res.status(200).json({ error: false, message: "Check Your Email For A confirmation Link" });
-  }
-})
+</body>`,
+      },
+      (err: any, info: any) => {
+        if (err) {
+          res
+            .status(500)
+            .json({ error: true, message: "Something went wrong" });
+        } else {
+          res
+            .status(200)
+            .json({
+              error: false,
+              message: "Check Your Email For A confirmation Link",
+            });
+        }
+      }
+    );
   } else {
     res.status(400).json({ error: true, message: "Something went wrong" });
   }
 };
 
-const complete = async (req: Req, res: Response) => {
-  const { client_id } = req.query;
+const completeSignUp = async (req: Req, res: Response) => {
+  const { client_id, gender, LGA, username, phone } = req.body;
   const pass = await Pass.findOne({
-    user_id: client_id,
+    confirmation_id: client_id,
   });
   if (!pass) {
     return res.status(400).json({ error: true, message: "User not found" });
   } else {
-    
+      pass.delete();
+    if (pass.date < Date.now()) {
+      return res.status(400).json({ error: true, message: "Link Expired" });
+    } else {
+      const user = await User.findOne({
+        id: pass.user_id,
+      });
+      if (!user) {
+        return res.status(400).json({ error: true, message: "User not found" });
+      } else {
+        console.log(user.confirmed)
+        if (user.confirmed) {
+          return res
+            .status(400)
+            .json({ error: true, message: "User already verified" });
+        } else {
+          user.gender = gender;
+          user.LGA = LGA;
+          user.username = username;
+          user.phone = phone;
+          user.confirmed = true;
+          await user.save();
+          const token = jwt.sign({ userId: user.id }, jwtSecret || "", {
+            expiresIn: "30d",
+          });
+          req.session.token = token;
+          return res
+            .status(200)
+            .json({ error: false, message: "Account Verified", token });
+        }
+      }
+    }
   }
-}
+};
 
 const forgotPassword = async (req: Req, res: Response) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
+  
   if (!user) {
     return res.status(400).json({ error: true, message: "User not found" });
   } else {
-    sendMail({
-      to: email,
-      subject: "Password Reset",
-      value: `
+    const confirmation_id = uuidv4();
+    const ONE_DAY = 24 * 60 * 60 * 1000;
+    const pass = new Pass({
+      user_id: user.id,
+      confirmation_id,
+      date: Date.now() + ONE_DAY,
+    });
+    await pass.save();
+    sendMail(
+      {
+        to: email,
+        subject: "Password Reset",
+        value: `
 <div style='padding: 0;background-color: #FFDE4E;display: grid;place-items: center;font-family: 'Chivo Mono', monospace;'>
   <h1 style='text-align: center;font-size: 35px;margin-bottom: 20px;'>Edobase</h1>
   <section style='background: white;width: 80%;margin: auto;height: fit-content;display: block;position: relative;padding: 25px;'>
     <p style='font-weight: 700'> Dear [Name],</p>
-    <p>Thank you for signing up for our forum! We're excited to have you as a member of our community.<br />To complete your registration and activate your account, please click on the following link:</p>
-    <div><a href=''>[Confirmation Link]</a></div>
-    <div><code>The Link is valid for 24 hours <a href=''> Click here to generate another one</a></code></div>
-    <p>Once you've clicked on the link, your account will be activated and you'll be able to start participating in discussions and connecting with other members.</p>
-
+    <p>Follow this <a>link</a> to reset your password</p>
+    <div><a href='${confirmation_id}'>${confirmation_id}"</a></div>
     <p>Thank you for joining us, and we look forward to seeing you on the forum!</p>
   </section>
 </div>
 </body>
-    ` 
-    }, (err: any, info: any) => {
-      if (err) {
-        res.status(500).json({ error: true, message: "Something went wrong" });
+    `,
+      },
+      (err: any, info: any) => {
+        if (err) {
+          res
+            .status(500)
+            .json({ error: true, message: "Something went wrong" });
+        } else {
+          res
+            .status(200)
+            .json({ error: false, message: "Password reset link sent" });
+        }
       }
-      else {
-        res.status(200).json({ error: false, message: "Password reset link sent" });
-      }
-    })
+    );
   }
-}
+};
 
 const resetPassword = async (req: Req, res: Response) => {
-  const { user_id, password } = req.body;
-  const user = await User.findOne({
-    id: user_id,
+  const { client_id, password } = req.body;
+  const pass = await Pass.findOne({
+    confirmation_id: client_id,
   });
-  if (!user) {
-    return res.status(400).json({ error: true, message: "User not found" });
-  }
-  const hashedPassword = await hashing(password) as string;
-  if (hashedPassword) {
-    user.password = hashedPassword;
-    await user.save();
-    res.status(200).json({ error: false, message: "Password reset successful" });
+  if (!pass) {
+    return res.status(400).json({ error: true, message: "User not found 2" });
   } else {
-    res.status(400).json({ error: true, message: "Something went wrong" });
+    pass.delete();
+    if (pass.date < Date.now()) {
+      return res.status(400).json({ error: true, message: "Link Expired" });
+    } else {
+      const user = await User.findOne({
+        id: pass.user_id,
+      });
+      if (!user) {
+        return res.status(400).json({ error: true, message: "User not found 1" });
+      } else {
+        if (user.confirmed) {
+          const hashedPassword = (await hashing(password)) as string;
+          if (hashedPassword) {
+            user.password = hashedPassword;
+            await user.save();
+            res
+              .status(200)
+              .json({ error: false, message: "Password reset successful" });
+          } else {
+            res.status(400).json({ error: true, message: "Something went wrong" });
+          }
+        } else {
+          return res
+            .status(400)
+            .json({ error: true, message: "User already verified" });
+        }
+      }
+    }
   }
-}
+};
 
-export { login, signUp, resetPassword, forgotPassword };
+export { login, signUp, resetPassword, forgotPassword, completeSignUp };
