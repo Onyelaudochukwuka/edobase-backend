bfcad01c5cfea3b7b689676f8f343a9cd7ffb1e3
Udoka <udochukwukaonyela@gmail.com>
2022-12-28 21:37:37 +0100

    feat: added middleware

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,8 +1,9 @@
 import express, { Response, Request, NextFunction, IRouter } from 'express';
+import { isAuthorized } from "../middleware";
 const router: IRouter = express.Router();
 
 /* GET home page. */
-router.get('/', function (req: Request, res: Response , next: NextFunction) {
+router.get('/', isAuthorized, function (req: Request, res: Response , next: NextFunction) {
   res.json({ title: 'Express' });
 });
 
