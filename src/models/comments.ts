e755e587425c9ed8a04ee6c7778c74de988a512e
Udoka <udochukwukaonyela@gmail.com>
2022-12-29 19:03:23 +0100

    feat created comment model

---
+++ src/models/comments.ts
@@ -0,0 +1,30 @@
+import { Schema, model, models } from "mongoose";
+interface Comments {
+  id: string;
+  date: Date;
+  content: string;
+  likes: number;
+  dislikes: number;
+  ref: string;
+  reports: {
+    type: string;
+    reason: string;
+    frequency: number;
+  } [];
+};
+
+const commentSchema = new Schema<Comments>({
+  ref: { type: String, required: true },
+  id: { type: String, required: true },
+  date: { type: Date, required: true },
+  content: { type: String, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  reports: [
+    {
+      type: { type: String, required: true },
+      reason: { type: String, required: true },
+      frequency: { type: Number, required: true },
+    },
+  ],
+});
