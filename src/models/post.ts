f5a9cdc1b7fd9ef8e6aae4517457a1d79102c820
Udoka <udochukwukaonyela@gmail.com>
2022-12-29 19:03:33 +0100

    feat: created post model

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,7 +1,31 @@
 import { Schema, model, models } from "mongoose";
 export interface IPost {
+  id: string;
+  date: Date;
+  content: string;
+  likes: number;
+  views: number;
+  dislikes: number;
+  reports: {
+    type: string;
+    reason: string;
+    frequency: number;
+  }[];
+  comments: number;
 }
 const postSchema = new Schema<IPost>({
-  
+  id: { type: String, required: true },
+  date: { type: Date, required: true },
+  content: { type: String, required: true },
+  likes: { type: Number, required: true },
+  views: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  reports: [
+    {
+      type: { type: String, required: true },
+      reason: { type: String, required: true },
+      frequency: { type: Number, required: true },
+    },
+  ],
 });
 export default models.Post || model("Post", postSchema);
