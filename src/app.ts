cbcba507d997bb88d638f17dcc5d74a41253b298
Udoka <udochukwukaonyela@gmail.com>
2023-01-05 21:59:11 +0100

    feat: modified configurations

--- src/app.ts
+++ src/app.ts
@@ -9,30 +9,8 @@ const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
-const MongoStore = require('connect-mongo');
 const app: Application = express();
 const Options = { limit: "10mb", extended: true };
-const oneDay = 1000 * 60 * 60 * 24;
-const sessions = require('express-session');
-const advancedOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-};
-const uri = process.env.MONGODB_URI;
-if (!uri) {
-  throw new Error("uri missing");
-}
-app.use(sessions({
-  secret: "abddb800afb24c08fed20f0aad14512138fbccc4b8d0be2ff9f3d57c12fe85d9506c37d13a64714a11c2aeaced38317d2dd31e2c2d49be6dd3778691fe551035334669f56eca6e4ccd81e5aef4a370a2ab6f0a2dc2dbd152324ba80e4082fa39c0b5b66459002173593800d8ec609dfebdd86d3f6ae88cd77298fb2e71e655d9",
-  saveUninitialized: true,
-  cookie: { maxAge: 30 * oneDay },
-  resave: false,
-  store: MongoStore.create({
-    mongoUrl: uri,
-    mongoOptions: advancedOptions
-  }), 
-}));
-
 // view engine setup
 app.use(logger('dev'));
 app.use(express.json());
