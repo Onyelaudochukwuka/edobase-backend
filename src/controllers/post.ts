5fb981700f66e2d48dfa560b6b5e2b663535d29d
Udoka <udochukwukaonyela@gmail.com>
2023-01-12 23:18:32 +0100

    feat: added new rouytes

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -1,45 +1,56 @@
 import { Request, Response } from "express";
-import { IPost, Post } from "../models";
-import { Error } from "mongoose";
+import { IPost, Post, User } from "../models";
+import { CallbackError, Error } from "mongoose";
 
 const home = (req: Request, res: Response) => {
     const posts = Post.find({
-        promoted: true
+        promoted: true,
     });
     if (!posts) {
         res.status(404).json({
             error: true,
-            message: "No post found"
+            message: "No post found",
         });
     } else {
         res.json({
             error: false,
-            data: posts
+            data: posts,
         });
     }
 };
 
 const create = (req: Request, res: Response) => {
-    const { title, content, author, promoted } = req.body;
+    const { title, content, author } = req.body;
     const post = new Post({
         title,
         content,
         author,
-        promoted
     });
     post.save((err: Error, post: IPost) => {
-        if (err) {
-            res.status(500).json({
-                error: true,
-                message: "Something went wrong"
-            });
-        } else {
-            res.status(200).json({
-                error: false,
-                message: "Post created successfully",
-                data: post
-            });
-        }
+        User.findOneAndUpdate(
+            {
+                id: author,
+            },
+            {
+                $push: {
+                    posts: post._id,
+                },
+            },
+            (err: Error) => {
+                if (err) {
+                    res.status(500).json({
+                        error: true,
+                        message: "Something went wrong",
+                    });
+                } else {
+                    res.status(200).json({
+                        error: false,
+                        message: "Post created successfully",
+                        data: post,
+                    });
+                }
+            }
+        );
     });
 };
 
@@ -72,4 +83,26 @@ const update = (req: Request, res: Response) => {
         }
     );
 };
-export { home, create, update };
+
+const getPost = (req: Request, res: Response) => {
+    const { id } = req.params;
+    Post.findOne({
+        _id: id,
+    })
+        .populate("author")
+        .exec((err: CallbackError, post: IPost) => {
+            if (err) {
+                res.status(500).json({
+                    error: true,
+                    message: "Something went wrong",
+                });
+            } else {
+                res.status(200).json({
+                    error: false,
+                    message: "Post fetched successfully",
+                    data: post,
+                });
+            }
+        });
+};
+export { home, create, update, getPost };
