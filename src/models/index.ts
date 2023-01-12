7f66af06d05ae3d189e7a9899305e46d7c66c0d2
Udoka <udochukwukaonyela@gmail.com>
2023-01-12 10:06:13 +0100

    feat: exported schemas

---
+++ src/models/index.ts
@@ -0,0 +1,8 @@
+export { default as Post } from './post';
+export { default as User } from './user';
+export { default as Comments } from './comments';
+export { default as Pass } from './pass';
+export * from './post';
+export * from './user';
+export * from './comments';
+export * from './pass';
