f45891d51ee4eb2f5d1497db4525f245436a82c1
Udoka <udochukwukaonyela@gmail.com>
2023-01-14 17:43:52 +0100

    feat: remove unneeded queries

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,5 +1,5 @@
 import express, { IRouter } from 'express';
-import { create, home } from "../controllers/post";
+import { create, getPost, home, update } from "../controllers/post";
 import { body, param } from "express-validator";
 import { Validate, isAuthorized } from "../middleware";
 const router: IRouter = express.Router();
@@ -7,31 +7,27 @@ const router: IRouter = express.Router();
 /* GET home page. */
 router.get('/', home);
 
-router.post('/create',
+router.post('/create-post',
     body('title').trim().not().isEmpty().withMessage('Title is required.'),
     body('content').trim().not().isEmpty().withMessage('Content is required.'),
     body('author').trim().not().isEmpty().withMessage('Author is required.'),
+    body('topic').trim().not().isEmpty().withMessage('Topic is required.'),
     Validate,
     isAuthorized,
     create
 );
 
 router.post('/update/:id',
-    body('title').trim().not().isEmpty().withMessage('Title is required.'),
-    body('content').trim().not().isEmpty().withMessage('Content is required.'),
-    body('author').trim().not().isEmpty().withMessage('Author is required.'),
-    body('promoted').trim().not().isEmpty().withMessage('Promoted is required.'),
     param('id').trim().not().isEmpty().withMessage('ID is required.'),
     Validate,
     isAuthorized,
-    create
+    update,
 );
-
 router.get('/get-post/:id',
     param('id').trim().not().isEmpty().withMessage('ID is required.'),
     Validate,
     isAuthorized,
-    create
+    getPost,
 );
 
 export default router;
